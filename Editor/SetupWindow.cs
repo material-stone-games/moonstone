92f5f4991c250db91d4c9e993a96d4ad187e4ccf
YouHyunwoo <dbgusdn0922@naver.com>
2025-08-18 18:47:40 +0900

    아이콘 이미지 추가 및 SetupWindow UI 개선, package.json 버전 0.5.1로 업데이트

--- Editor/SetupWindow.cs
+++ Editor/SetupWindow.cs
@@ -7,15 +7,54 @@ namespace Moonstone
 {
     public class SetupWindow : EditorWindow
     {
+        Texture2D icon;
+        string windowName = "Moonstone";
+
         [MenuItem("Window/Moonstone/Project Setup")]
         public static void ShowWindow()
         {
             GetWindow<SetupWindow>("Project Setup");
         }
 
-        private void OnGUI()
+        void OnEnable() {
+            icon = AssetDatabase.LoadAssetAtPath<Texture2D>($"Packages/{Constants.PACKAGE_NAME}/Editor/Data/icon.png");
+        }
+
+        void OnGUI()
         {
+            GUILayout.BeginHorizontal(GUI.skin.box, GUILayout.Height(100), GUILayout.ExpandWidth(true));
+            GUILayout.FlexibleSpace();
+
+            GUILayout.BeginVertical();
+            GUILayout.FlexibleSpace();
+
+            GUILayout.BeginHorizontal();
+            // 아이콘
+            if (icon != null)
+            {
+                GUIStyle centeredIconStyle = new GUIStyle();
+                centeredIconStyle.alignment = TextAnchor.MiddleCenter;
+                GUILayout.Label(icon, centeredIconStyle, GUILayout.Width(64), GUILayout.Height(64));
+            }
+
+            // 이름
+            GUIStyle nameStyle = new GUIStyle(EditorStyles.label);
+            nameStyle.fontSize = 24;
+            nameStyle.alignment = TextAnchor.MiddleCenter;
+            nameStyle.fontStyle = FontStyle.Bold;
+
+            GUILayout.Label(windowName, nameStyle, GUILayout.Height(64));
+
+            GUILayout.EndHorizontal();
+
+            GUILayout.FlexibleSpace();
+            GUILayout.EndVertical();
+
+            GUILayout.FlexibleSpace();
+            GUILayout.EndHorizontal();
+
             GUILayout.Label("Project Structure Template", EditorStyles.boldLabel);
+            GUILayout.Label("This template sets up a basic project structure within the Assets folder.", EditorStyles.wordWrappedLabel);
             GUILayout.Space(10);
 
             if (GUILayout.Button("Apply"))
@@ -24,7 +63,7 @@ namespace Moonstone
             }
         }
 
-        private void ApplyTemplate()
+        void ApplyTemplate()
         {
             var packageInfo = UnityEditor.PackageManager.PackageInfo.FindForPackageName(Constants.PACKAGE_NAME);
             if (packageInfo == null)
@@ -48,7 +87,7 @@ namespace Moonstone
             Debug.Log("✅ Complete applying project structure template!");
         }
 
-        private void CopyDirectory(string sourceDir, string targetDir)
+        void CopyDirectory(string sourceDir, string targetDir)
         {
             Directory.CreateDirectory(targetDir);
 
